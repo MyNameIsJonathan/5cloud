e106d668b06eb520cefa4ce1f281033f13a65d75
Jonathan <jonathanholson@gmail.com>
2019-10-16 17:17:02 -0700

    Further Attempts to Configure Jest

--- songDisplayComponent/src/App.spec.js
+++ songDisplayComponent/src/App.spec.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-import App from './app';
+const React = require('react');
+const renderer = require('react-test-renderer');
+const App = require('../public/bundle.js');
 
 test('Link changes the class when hovered', () => {
-  const component = renderer.create(<App></App>);
+  const component = renderer.create(<App />);
   let tree = component.toJSON();
   expect(tree).toMatchSnapshot();
 });
